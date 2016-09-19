0a59a846cc4ccdf94409b677cca0847d45815907
luisff2016 <luisff@dcomp.ufs.br>
2016-09-19 19:20:18 -0300

    versao 1.01
    lab dcomp 19:20

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int cpf;
 char nome[50];
 char sexo;
 float notas[4];
+float media;
 } TESTUDANTE;
 
 /* definindo variaveis globais */
@@ -17,13 +18,14 @@ int codCadastro;
 TESTUDANTE aluno[50];
 TESTUDANTE est;
 
-//estou dentro.
 /* funcoes auxiliares */
 void menu();
 int buscaEstudante();
 void lerAluno();
 void gravarAluno();
 void addMenu();
+void verMatAluno();
+void veRegistro();
 
 /* funcao principal */
 int main(){
@@ -32,23 +34,6 @@ int main(){
     return 0;
 }
 
-/* exibe menu de opcoes */
-void menu (){
-printf("============================================\n");
-printf("\t\tMenu\t\t\n");
-printf("============================================\n");
-printf ("1. Adicione registro de estudante\n");
-printf ("2. Apague registro de estudante\n");
-printf ("3. Atualize registro de estudante\n");
-printf ("4. Ver todos os registros\n");
-printf ("5. Calcular a m√©dia de um estudante\n");
-printf ("6. Mostrar o estudante que conseguiu a maior m√©dia\n");
-printf ("7. Mostrar o estudante com a menor m√©dia\n");
-printf ("8. Encontrar estudante pela matr√≠cula\n");
-printf ("9. Encontrar estudante pelo nome\n");
-printf ("10. Mostrar os registros ordenados pela m√©dia\n");
-printf ("0. Sair do programa\n");
-}
 
 /* grava o registro do estudante */
 void gravarAluno(TESTUDANTE registrar, int a){
@@ -71,9 +56,9 @@ void lerAluno(){
     scanf("%d", &est.cpf);
     int i = buscaEstudante(est);
     if ( i != -1 )
-        printf("\nMatr√≠cula j√° cadastrada\n");
+        printf("\nMatrÌcula j· cadastrada\n");
     else if (codCadastro == nroMaxAlunos)
-        printf("\nN√∫mero m√°ximo de estudantes ultrapassado\n");
+        printf("\nN˙mero m·ximo de estudantes ultrapassado\n");
         else {
             printf("\nDigite o nome do aluno:\n");
             scanf("%s", est.nome);
@@ -81,8 +66,15 @@ void lerAluno(){
             est.sexo = getche();
             printf("\nDigite as notas:\n");
             int i;
-            for (i=0; i<4; i++)
+            float soma=0;
+            for (i=0; i<4; i++){
                 scanf("%f", &est.notas[i]);
+<<<<<<< HEAD
+                soma += est.notas[i];
+            }
+            est.media = soma/4;
+=======
+>>>>>>> origin/miniprojeto
             gravarAluno(est, codCadastro);
             printf("\nCadastrado realizado com sucesso\n");
     }
@@ -97,7 +89,8 @@ void veRegistro(){
         printf("NOTA 1: %f  ", aluno[i].notas[0]);
         printf("NOTA 2: %f  ", aluno[i].notas[1]);
         printf("NOTA 3: %f  ", aluno[i].notas[2]);
-        printf("NOTA 4: %f\n", aluno[i].notas[3]);
+        printf("NOTA 4: %f  ", aluno[i].notas[3]);
+        printf("MEDIA: %f\n", aluno[i].media);
     }
 }
 
@@ -124,14 +117,37 @@ void altRegistro(){
     scanf("%d", &est.cpf);
     int i;
     for (i=0; i<codCadastro; i++){
+<<<<<<< HEAD
         if (aluno[i].cpf == est.cpf) {
+=======
+        if (aluno[i].cpf == a.cpf) {
+                for( ; i<codCadastro-1 ; i++) aluno[i]=aluno[i+1];
+                codCadastro--;
+            }
+    }
+    printf("\n Registro apagado \n");
+}
+
+void altRegistro(){
+    printf("\n Qual matricula deseja alterar?\n\n");
+    scanf("%d", &est.cpf);
+    int i;
+    for (i=0; i<codCadastro; i++){
+        if (aluno[i].cpf == est.cpf) {
+>>>>>>> origin/miniprojeto
             printf("\nDigite o nome do aluno:\n");
             scanf("%s", est.nome);
             printf("\nDigite o sexo: H ou M\n");
             est.sexo=getche();
             printf("\nDigite as Notas:\n");
             int j;
-            for (j=0; j<4; j++) scanf("%f", &est.notas[j]);
+<<<<<<< HEAD
+            float soma=0;
+            for (j=0; j<4; j++){
+                scanf("%f", &est.notas[j]);
+                soma += est.notas[j];
+            }
+            est.media = soma/4;
             gravarAluno(est, i);
             codCadastro--;
             printf("\nCadastrado alterado com sucesso\n");
@@ -139,11 +155,37 @@ void altRegistro(){
     }
 }
 
-/* Menu de sele√ß√£o */
+/* exibe menu de opcoes */
+void menu (){
+printf("============================================\n");
+printf("\t\tMenu\t\t\n");
+printf("============================================\n");
+printf ("1. Adicione registro de estudante\n");
+printf ("2. Apague registro de estudante\n");
+printf ("3. Atualize registro de estudante\n");
+printf ("4. Ver todos os registros\n");
+printf ("5. Calcular a mÈdia de um estudante\n");
+printf ("6. Mostrar o estudante que conseguiu a maior mÈdia\n");
+printf ("7. Mostrar o estudante com a menor mÈdia\n");
+printf ("8. Encontrar estudante pela matrÌcula\n");
+printf ("9. Encontrar estudante pelo nome\n");
+printf ("10. Mostrar os registros ordenados pela mÈdia\n");
+printf ("0. Sair do programa\n");
+=======
+            for (j=0; j<4; j++) scanf("%f", &est.notas[j]);
+            gravarAluno(est, i);
+            codCadastro--;
+            printf("\nCadastrado alterado com sucesso\n");
+        }
+    }
+>>>>>>> origin/miniprojeto
+}
+
+/* Menu de seleÁ„o */
 void addMenu(){
     menu();
     int opcao;
-    printf("\nDigite a op√ß√£o desejada do MENU\n\n");
+    printf("\nDigite a opÁ„o desejada do MENU\n\n");
     scanf("%d", &opcao);
     while (opcao != 0){
             switch (opcao){
@@ -157,11 +199,11 @@ void addMenu(){
                 case 7: printf("\n opcao em construcao\n\n"); break;
                 case 8: verMatAluno(); break;
                 case 9: printf("\n opcao em construcao\n\n"); break;
-                case 10: printf("\n opcao em construcao\n\n"); break;
+                case 10: verMedia(); break;
                 default: printf("\n DIGITE UMA OPCAO VALIDA!\n\n"); break;
                 }
             menu();
-            printf("\n Digite a op√ß√£o desejada do MENU\n\n");
+            printf("\n Digite a opÁ„o desejada do MENU\n\n");
             scanf("%d", &opcao);
             }
 }
@@ -171,20 +213,7 @@ void verMatAluno(){
     scanf("%d", &est.cpf);
     int i = buscaEstudante(est);
     if ( i != -1 ){
-        if (aluno[j].cpf == est.cpf) {
-            printf("CPF: %d  ", aluno[i].cpf);
-            printf("NOME: %s  ", aluno[i].nome);
-            printf("SEXO: %c  ", aluno[i].sexo);
-            printf("NOTA 1: %f  ", aluno[i].notas[0]);
-            printf("NOTA 2: %f  ", aluno[i].notas[1]);
-            printf("NOTA 3: %f  ", aluno[i].notas[2]);
-            printf("NOTA 4: %f\n", aluno[i].notas[3]);
-        }
-    }
-    else printf("\n Registro nao existe\n");
-
-    int i;
-    for (i=0; i<codCadastro; i++){
+        for (i=0; i<codCadastro; i++){
         if (aluno[i].cpf == est.cpf) {
             printf("CPF: %d  ", aluno[i].cpf);
             printf("NOME: %s  ", aluno[i].nome);
@@ -193,6 +222,19 @@ void verMatAluno(){
             printf("NOTA 2: %f  ", aluno[i].notas[1]);
             printf("NOTA 3: %f  ", aluno[i].notas[2]);
             printf("NOTA 4: %f\n", aluno[i].notas[3]);
+            }
         }
     }
+    else printf("\n Registro nao existe\n");
+}
+
+void verMedia(){
+    int i,h;
+    for (i=0; i<codCadastro; i++){
+            est.media=0;
+            for (h=0; h<codCadastro ; h++){
+            if ( aluno[h].media>=est.media) est.media=aluno[h].media;
+            printf("MEDIA: %f\n", est.media);
+            }
+        }
 }
